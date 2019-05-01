3b9dee36828bb889a6576a3e436e63449c553b91
Eddie Chu <contact@eddiechu.me>
2019-05-01 00:45:37 -0700

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css'
+
+export default class Footer extends React.Component {
+
+    render() {
+        return (
+            <footer>
+                <section>1</section>
+                <section>2</section>
+                <section>3</section>
+            </footer>
+        )
+    }
+}
+
+
