b9ae95b0a0c5875ffa61053fa46146fb80e09375
Eddie Chu <contact@eddiechu.me>
2019-05-05 21:52:23 -0700

    update files

--- src/components/MovieBoard/MovieBoard.js
+++ src/components/MovieBoard/MovieBoard.js
@@ -16,14 +16,14 @@ export default class MovieBoard extends React.Component {
                 <div className="title-section">
                     <h2>Reel Reviews</h2>
                     <p>Browse and rate the latest and greatest movies.</p>
-                    <Link to="/search"><button>Get Started</button></Link>
+                        <Link to="/search/?query=avengers"><button>Get Started</button></Link>
                 </div>
-                {movieBoardItems[2]}
+                    {movieBoardItems[5]}
                 </div>
                 <div className="bot">
-                    <div className="bLeft">{movieBoardItems[6]}</div>
-                    <div className="bMid">{movieBoardItems[4]}</div>
-                    <div className="bRight">{movieBoardItems[7]}</div>
+                    <div className="bLeft">{movieBoardItems[8]}</div>
+                    <div className="bMid">{movieBoardItems[6]}</div>
+                    <div className="bRight">{movieBoardItems[2]}</div>
                 </div>
                
             </div>
--- src/components/MovieBoardItem/MovieBoardItem.js
+++ src/components/MovieBoardItem/MovieBoardItem.js
@@ -4,6 +4,13 @@ import './MovieBoardItem.css';
 export default function MovieBoardItem(props) {
     return (
         <div className="movieBoardItem">
+            <div className="movieBoard-desc">
+                <div className="inner-desc">
+                    <h4>{props.data.original_title}</h4>
+                    <p>{props.data.overview}</p>
+                </div>
+              
+            </div>
             <img className="inner-movieBoardItem" src={`https://image.tmdb.org/t/p/original/${props.data.backdrop_path}`} alt={props.data.title} />
         </div>
     );
