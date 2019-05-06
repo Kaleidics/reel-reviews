d9d85b016bb92020e65a4eafd8df346fc2e6ae0b
Eddie Chu <contact@eddiechu.me>
2019-05-05 20:56:56 -0700

    update files

--- src/components/MovieBoard/MovieBoard.js
+++ src/components/MovieBoard/MovieBoard.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import MovieBoardItem from '../MovieBoardItem/MovieBoardItem';
+import { Link } from 'react-router-dom';
 import './MovieBoard.css';
 
 export default class MovieBoard extends React.Component {
     render() {
 
-        const movieBoardItems = this.props.movies.slice(4,8).map((item, index ) => {
+        const movieBoardItems = this.props.movies.slice(4,13).map((item, index ) => {
             return <MovieBoardItem data={item} index={index} key={this.props.movies[index].id} />
         });
+        
         return (
             <div className="outer-MovieBoard">
-                <div className="topMid">{movieBoardItems[2]}</div>
+                <div className="topMid">
+                <div className="title-section">
+                    <h2>Reel Reviews</h2>
+                    <p>Browse and rate the latest and greatest movies.</p>
+                    <Link to="/search"><button>Get Started</button></Link>
+                </div>
+                {movieBoardItems[2]}
+                </div>
                 <div className="bot">
-                    <div className="bLeft">{movieBoardItems[3]}</div>
-                    <div className="bMid">{movieBoardItems[0]}</div>
-                    <div className="bRight">{movieBoardItems[1]}</div>
+                    <div className="bLeft">{movieBoardItems[6]}</div>
+                    <div className="bMid">{movieBoardItems[4]}</div>
+                    <div className="bRight">{movieBoardItems[7]}</div>
                 </div>
                
             </div>
