bd225c6eb544c9543b8afb4a4b3738eb606103c4
Eddie Chu <contact@eddiechu.me>
2019-05-18 00:53:17 -0700

    style changes

--- src/components/Actors/Actors.js
+++ src/components/Actors/Actors.js
@@ -16,7 +16,7 @@ export class Actors extends React.Component {
         if (this.props.data) {
             return (
                 <div className="actor-Container" onClick={this.handleId}>
-                    <img className="actor-picture" src={`https://image.tmdb.org/t/p/w500/${this.props.data.profile_path}`} alt={this.props.data.name} />
+                    <img className="actor-picture" src={`https://image.tmdb.org/t/p/w500/${this.props.data.profile_path}`} alt={this.props.data.name} style={{ background: this.props.data.profile_path == null ? '#333' : 'transparent' }} />
                     <h4 className="character-name">{this.props.data.character}</h4>
                     <p className="actor-name">{this.props.data.name}</p>
                 </div>
