b60f4963e19ea0732331c5ad30b94f5d0d51fea6
Eddie Chu <contact@eddiechu.me>
2019-05-21 23:49:32 -0700

    update responsive results page

--- src/components/Cast/Settings.js
+++ src/components/Cast/Settings.js
@@ -29,7 +29,7 @@ export const settings = {
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 800,
           settings: {
             slidesToShow: 3.3,
             slidesToScroll: 3,
