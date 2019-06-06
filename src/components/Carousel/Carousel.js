5925eeb857200ede513b9ac955166ac0b37083c6
Eddie Chu <contact@eddiechu.me>
2019-06-05 23:42:05 -0700

    prettier

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,39 +1,31 @@
-import React from 'react';
-import CarouselItem from '../CarouselItem/CarouselItem';
-import { settings } from './Settings';
-import './Carousel.css';
+import React from "react";
+import CarouselItem from "../CarouselItem/CarouselItem";
+import { settings } from "./Settings";
+import "./Carousel.css";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export class Carousel extends React.Component {
+    componentDidMount() {
+        // the trick
+        setTimeout(() => {
+            this.forceUpdate();
+        }, 50);
+    }
 
-  componentDidMount() {
-    // the trick
-    setTimeout(() => {
-      this.forceUpdate();
-    }, 50);
-  }
-
-    render(){
-
+    render() {
         const carousel = this.props.movies.map((item, index) => {
-            return (
-                <CarouselItem data={item} index={index} key={this.props.movies[index].id} id={this.props.movies[index].id} />
-            );
+            return <CarouselItem data={item} index={index} key={this.props.movies[index].id} id={this.props.movies[index].id} />;
         });
 
         return (
-            <section className="inner-carousel" role="region" aria-labelledby={this.props.axeRegion}>
-            <h2 id={this.props.axeRegion}>{this.props.title}</h2>
-                <Slider {...settings}>
-                    {carousel}
-                </Slider>
-                <hr className="carousel-divider"/>
+            <section className="inner-carousel" aria-labelledby={this.props.axeRegion}>
+                <h2 id={this.props.axeRegion}>{this.props.title}</h2>
+                <Slider {...settings}>{carousel}</Slider>
+                <hr className="carousel-divider" />
             </section>
         );
     }
 }
-
-
--- src/components/DetailSummary/DetailSummary.js
+++ src/components/DetailSummary/DetailSummary.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Cast } from '../Cast/Cast';
-import './DetailSummary.css';
-import placeholder from '../../assets/movie_placeholder.png';
+import React from "react";
+import { Cast } from "../Cast/Cast";
+import "./DetailSummary.css";
+import placeholder from "../../assets/movie_placeholder.png";
 
 export default class DetailSummary extends React.Component {
-
     render() {
-
         const movie = this.props.data;
         let splash = !movie.poster_path ? <img className="splash" src={placeholder} alt={"placeholder"} /> : <img className="splash" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />;
 
-            //Computing average score for a movie, needs refactor and possibly export own file
-             let score = 0;
-             for (let i=0; i <this.props.reviewData.length; i++) {
-                score = score + this.props.reviewData[i].reviewScore;
-             }
-             let avgScore = score/this.props.reviewData.length;
+        //Computing average score for a movie, needs refactor and possibly export own file
+        let score = 0;
+        for (let i = 0; i < this.props.reviewData.length; i++) {
+            score = score + this.props.reviewData[i].reviewScore;
+        }
+        let avgScore = score / this.props.reviewData.length;
 
         return (
             <div className="summary-container">
@@ -24,25 +22,31 @@ export default class DetailSummary extends React.Component {
                     <div className="summary-details">
                         <ul className="summary-details-left">
                             <li className="release">Original Title: {movie.original_title}</li>
-                            <li className="original-lang">Original Language: <span>{movie.original_language}</span></li> 
+                            <li className="original-lang">
+                                Original Language: <span>{movie.original_language}</span>
+                            </li>
                             <br />
                             <li className="runtime">Runtime: {movie.runtime}mins</li>
                             <li className="release">Release Date: {movie.release_date}</li>
                         </ul>
                         <ul className="summary-details-right">
-                            <li className="rr-score"><span>RR Score:</span><span>{ avgScore ? `${avgScore.toFixed(1)} / 5` : 'N/A' }</span></li>
-                            <li className="imdb-score"><span>TMDB Score:</span><span>{movie.vote_average} / 10</span></li>
+                            <li className="rr-score">
+                                <span>RR Score:</span>
+                                <span>{avgScore ? `${avgScore.toFixed(1)} / 5` : "N/A"}</span>
+                            </li>
+                            <li className="imdb-score">
+                                <span>TMDB Score:</span>
+                                <span>{movie.vote_average} / 10</span>
+                            </li>
                         </ul>
                     </div>
                     <div className="summary-paragraph-container">
-                    <h3>Summary</h3>
+                        <h3>Summary</h3>
                         <p>{movie.overview}</p>
-                    </div>   
+                    </div>
                     <Cast title={"Starring"} cast={this.props.castdata} />
                 </div>
-                <div className="summary-inner-right">
-                    {splash}
-                </div>
+                <div className="summary-inner-right">{splash}</div>
             </div>
         );
     }
