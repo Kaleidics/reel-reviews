67111038d7a666d43eca7d83e270ae06388e12d9
Eddie Chu <contact@eddiechu.me>
2019-06-07 00:19:12 -0700

    remake review container and styling

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { createPost } from '../../actions/index';
 import './CreatePost.css';
 
-
 export class CreatePost extends React.Component {
     constructor(props) {
         super(props);
@@ -21,23 +20,27 @@ export class CreatePost extends React.Component {
     handleCreate = e => {
         e.preventDefault();
         this.props.createPost(this.state);
-        this.props.handleFn();
+        setTimeout(() => {this.props.handleFn()}, 500);
     }
 
     render() {
         return (
             <div className="review-form-container">
                 <form id="review-form" className="create-form" onSubmit={this.handleCreate}>
+                    <div className="review-exit" onClick={this.props.handleFn}>
+                        <div className="exit-icon1" />
+                        <div className="exit-icon2" />
+                    </div>
                     <label htmlFor="user-review-title">Title</label>
                     <input id="user-review-title" className="input-width" type="text" placeholder="" onChange={e => this.setState({ reviewTitle: e.target.value })} required />
                     <label htmlFor="user-review-text">Description</label>
-                    <textarea form ="review-form" className="input-width" placeholder="" maxLength="700" cols="15" onChange={e => this.setState({ reviewText: e.target.value })} ></textarea>
+                    <textarea form="review-form" className="input-width" placeholder="" maxLength="700" cols="15" onChange={e => this.setState({ reviewText: e.target.value })} />
                     <label htmlFor="user-review-rating">Rating</label>
                     <input id="user-review-rating" className="score-input" type="number" min="1" max="5" onChange={e => this.setState({ reviewScore: e.target.value })} required />
                     <button className="submit-create">Submit</button>
                 </form>
             </div>
-        )
+        );
     }
 }
 
